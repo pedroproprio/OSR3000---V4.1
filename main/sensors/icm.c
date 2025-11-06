383742261784824c7af17e33159ed76716cf5cda
joaoSallesr <joao.sallesr@gmail.com>
2025-11-06 14:05:50 -0300

    mudança x

--- main/sensors/icm.c
+++ main/sensors/icm.c
@@ -141,16 +141,16 @@ void fusion_task(data_t *data, FusionOffset *offset, FusionAhrs *ahrs, icm20948_
     xSemaphoreGive(xI2CMutex);
     // Update raw sensor data
     data->accel_x = agmt->acc.axes.x;
-    data->accel_x = agmt->acc.axes.y;
-    data->accel_x = agmt->acc.axes.z;
+    data->accel_y = agmt->acc.axes.y;
+    data->accel_z = agmt->acc.axes.z;
     
     data->gyro_x = agmt->gyr.axes.x;
-    data->gyro_x = agmt->gyr.axes.y;
-    data->gyro_x = agmt->gyr.axes.z;
+    data->gyro_y = agmt->gyr.axes.y;
+    data->gyro_z = agmt->gyr.axes.z;
     
     data->mag_x = agmt->mag.axes.x;
-    data->mag_x = agmt->mag.axes.y;
-    data->mag_x = agmt->mag.axes.z;
+    data->mag_y = agmt->mag.axes.y;
+    data->mag_z = agmt->mag.axes.z;
     //data->temperature = agmt->tmp.val;
     if(initial_temp == 0) initial_temp = (agmt->tmp.val*temp_scale + temp_offset) + 273;
 	// Acquire latest sensor data
